3c6cc9795537e8f716f2db1a63248f7ec0a7884c
Níkolas Schmitt <nikolas.schmitt@teclogica.com>
2019-12-17 13:13:01 -0300

    Funções de arrays
    Percorrendo array e gerando outro com o map(), transformando um array em
    um dado único com o reduce(), filtrando com o filter() e pesquisando com
    o find().

--- Skylab/ES6/bundle.js
+++ Skylab/ES6/bundle.js
@@ -1,26 +1,16 @@
 "use strict";
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
-
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
-
-var Matematica =
-/*#__PURE__*/
-function () {
-  function Matematica() {
-    _classCallCheck(this, Matematica);
-  }
-
-  _createClass(Matematica, null, [{
-    key: "soma",
-    value: function soma(a, b) {
-      return a + b;
-    }
-  }]);
-
-  return Matematica;
-}();
-
-console.log(Matematica.soma(1, 2));
+var arr = [1, 3, 4, 5, 8, 9];
+var newArr = arr.map(function (number, index) {
+  return number + index;
+});
+var sum = arr.reduce(function (total, next) {
+  return total + next;
+});
+var filter = arr.filter(function (number) {
+  return number % 2 === 0;
+});
+var find = arr.find(function (number) {
+  return number === 2;
+});
+console.log(find);
--- Skylab/ES6/main.js
+++ Skylab/ES6/main.js
@@ -1,7 +1,10 @@
-class Matematica {
-    static soma(a, b){
-        return a + b;
-    }
-}
+const arr = [1, 3, 4, 5, 8, 9];
 
-console.log(Matematica.soma(1, 2));
+const newArr = arr.map((number, index) => number + index);
+
+const sum = arr.reduce((total, next) => total + next);
+
+const filter = arr.filter(number => number % 2 === 0);
+
+const find = arr.find(number => number === 2);
+console.log(find);
